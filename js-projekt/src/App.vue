36cd3b578aa6d0eac4d74b9c5eb5a6fad2e4fd42
Skam4 <maksdziemian@gmail.com>
2024-01-25 22:06:53 +0100

    Przypisywanie Użytkowników do zadań

--- js-projekt/src/App.vue
+++ js-projekt/src/App.vue
@@ -1,151 +1,4 @@
-<script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
 
-const newListName = ref('');
-const newTask = ref({ title: '', description: '', status: 'not-done' });
-const taskLists = ref([]);
-const editingTaskIndex = ref(null);
-const editedTask = ref({ title: '', description: '', status: '' });
-
-const updateServerData = async () => {
-  try {
-    await axios.post('http://localhost:3001', { taskLists: taskLists.value, users: users.value });
-  } catch (error) {
-    console.error('Error updating data on the server', error);
-  }
-};
-
-
-const addTaskList = () => {
-  if (newListName.value.trim() !== '') {
-    taskLists.value.push({
-      name: newListName.value,
-      tasks: [],
-    });
-    newListName.value = '';
-    updateServerData();
-  }
-};
-
-const removeTaskList = (index) => {
-  taskLists.value.splice(index, 1);
-  updateServerData();
-};
-
-const addTask = (listIndex) => {
-  if (newTask.value.title.trim() !== '') {
-    taskLists.value[listIndex].tasks.push({
-      title: newTask.value.title,
-      description: newTask.value.description,
-      status: newTask.value.status,
-    });
-    newTask.value = { title: '', description: '', status: 'not-done' };
-    updateServerData();
-  }
-};
-
-const removeTask = (listIndex, taskIndex) => {
-  taskLists.value[listIndex].tasks.splice(taskIndex, 1);
-  updateServerData();
-};
-
-const saveEditedTask = (listIndex, taskIndex) => {
-  taskLists.value[listIndex].tasks[taskIndex].title = editedTask.value.title;
-  taskLists.value[listIndex].tasks[taskIndex].description = editedTask.value.description;
-  taskLists.value[listIndex].tasks[taskIndex].status = editedTask.value.status;
-  updateServerData();
-};
-
-const cancelEditingTask = () => {
-  editingTaskIndex.value = null;
-  editedTask.value = { title: '', description: '', status: '' };
-};
-
-
-
-const isModalVisible = ref(false);
-const isLoginVisible = ref(true);
-const loginData = ref({ email: '', password: '' });
-const registerData = ref({ firstName: '', lastName: '', email: '', password: '', nickname: '' });
-
-const showLoginModal = () => {
-  isModalVisible.value = true;
-  isLoginVisible.value = true;
-};
-
-const showRegisterModal = () => {
-  isModalVisible.value = true;
-  isLoginVisible.value = false;
-};
-
-const closeModal = () => {
-  isModalVisible.value = false;
-};
-
-const isLoggedIn = ref(false);
-
-const logout = () => {
-  isLoggedIn.value = false;
-};
-
-const login = () => {
-  const user = findUserByEmail(loginData.value.email);
-  if (user && user.password === loginData.value.password) {
-    isLoggedIn.value = true;
-    closeModal();
-  } else {
-    console.log('Nieprawidłowe dane logowania');
-  }
-};
-
-const register = () => {
-  const existingUser = findUserByEmail(registerData.value.email);
-  if (!existingUser) {
-    const newUser = {
-      id: getNextUserId(),
-      firstName: registerData.value.firstName,
-      lastName: registerData.value.lastName,
-      email: registerData.value.email,
-      password: registerData.value.password,
-      nickname: registerData.value.nickname,
-    };
-    users.value.push(newUser);
-    updateServerData();
-    closeModal();
-  } else {
-    console.log('Użytkownik już istnieje');
-  }
-};
-
-const userIdCounter = ref(1);
-
-const getNextUserId = () => {
-  const id = userIdCounter.value;
-  userIdCounter.value += 1;
-  return id;
-};
-
-const findUserByEmail = (email) => {
-  return users.value.find(user => user.email === email);
-};
-
-const users = ref([]);
-
-onMounted(async () => {
-  try {
-    const response = await axios.get('http://localhost:3001');
-    taskLists.value = response.data.taskLists;
-    users.value = response.data.users || [];
-  } catch (error) {
-    console.error('Error fetching data', error);
-  }
-});
-
-
-
-
-</script>
 
 <template>
   <div id="app">
@@ -209,8 +62,9 @@ onMounted(async () => {
               </select>
               <select v-model="newTask.assignedTo">
                 <option value="">Brak przypisania</option>
-                <option v-for="user in users" :value="user.id">{{ user.email }}</option>
+                <option v-for="user in users" :value="user.id">{{ user.firstName }} {{ user.lastName }}</option>
               </select>
+
 
               <button @click="addTask(index)">Dodaj</button>
             </div>
@@ -220,7 +74,7 @@ onMounted(async () => {
                   <strong>{{ task.title }}</strong>
                   <p>{{ task.description }}</p>
                   <p>Status: {{ task.status }}</p>
-                  <p v-if="task.assignedTo">Przypisane do: {{ findUserById(task.assignedTo).email }}</p>
+                  <p v-if="task.assignedTo">Przypisane do: {{ findUserById(task.assignedTo).firstName }} {{ findUserById(task.assignedTo).lastName }}</p>
                   <button @click="startEditingTask(index, taskIndex)">Edytuj</button>
                   <button @click="removeTask(index, taskIndex)">Usuń</button>
                 </span>
@@ -234,7 +88,7 @@ onMounted(async () => {
                   </select>
                   <select v-model="editedTask.assignedTo">
                     <option value="">Brak przypisania</option>
-                    <option v-for="user in users" :value="user.id">{{ user.email }}</option>
+                    <option v-for="user in users" :value="user.id">{{ user.firstName }} {{ user.lastName }}</option>
                   </select>
                   <button @click="saveEditedTask(index, taskIndex)">Zapisz</button>
                   <button @click="cancelEditingTask">Anuluj</button>
@@ -261,7 +115,7 @@ const editedTask = ref({ title: '', description: '', status: '', assignedTo: '' 
 const isModalVisible = ref(false);
 const isLoginVisible = ref(true);
 const loginData = ref({ email: '', password: '' });
-const registerData = ref({ email: '', password: '' });
+const registerData = ref({ firstName: '', lastName: '', email: '', password: '', nickname: '' });
 const isLoggedIn = ref(false);
 const users = ref([]);
 
@@ -352,8 +206,12 @@ const register = () => {
   const existingUser = findUserByEmail(registerData.value.email);
   if (!existingUser) {
     const newUser = {
+      id: getNextUserId(),
+      firstName: registerData.value.firstName,
+      lastName: registerData.value.lastName,
       email: registerData.value.email,
       password: registerData.value.password,
+      nickname: registerData.value.nickname,
     };
     users.value.push(newUser);
     updateServerData();
@@ -361,6 +219,13 @@ const register = () => {
   } else {
     console.log('Użytkownik już istnieje');
   }
+};
+
+const userIdCounter = ref(1);
+const getNextUserId = () => {
+  const id = userIdCounter.value;
+  userIdCounter.value += 1;
+  return id;
 };
 
 const findUserByEmail = (email) => {
