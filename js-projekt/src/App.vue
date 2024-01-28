aaca399a967c0c426101cda4f13e4b2f371458f1
Skam4 <maksdziemian@gmail.com>
2024-01-28 21:29:18 +0100

    Edytowanie tylko jednego zadania

--- js-projekt/src/App.vue
+++ js-projekt/src/App.vue
@@ -1,5 +1,3 @@
-
-
 <template>
   <div id="app">
     <div class="centered-text margin-bottom-small">
@@ -65,18 +63,17 @@
                 <option v-for="user in users" :value="user.id">{{ user.firstName }} {{ user.lastName }}</option>
               </select>
 
-
               <button @click="addTask(index)">Dodaj</button>
             </div>
             <ul>
-              <li class="task" v-for="(task, taskIndex) in list.tasks" :key="taskIndex">
-                <span v-if="taskIndex !== editingTaskIndex">
+              <li class="task" v-for="task in list.tasks" :key="task.id">
+                <span v-if="task.id !== editingTaskIndex">
                   <strong>{{ task.title }}</strong>
                   <p>{{ task.description }}</p>
                   <p>Status: {{ task.status }}</p>
                   <p v-if="task.assignedTo">Przypisane do: {{ findUserById(task.assignedTo).firstName }} {{ findUserById(task.assignedTo).lastName }}</p>
-                  <button @click="startEditingTask(index, taskIndex)">Edytuj</button>
-                  <button @click="removeTask(index, taskIndex)">Usuń</button>
+                  <button @click="startEditingTask(index, task.id)">Edytuj</button>
+                  <button @click="removeTask(index, task.id)">Usuń</button>
                 </span>
                 <span v-else>
                   <input v-model="editedTask.title" placeholder="Tytuł" />
@@ -90,7 +87,7 @@
                     <option value="">Brak przypisania</option>
                     <option v-for="user in users" :value="user.id">{{ user.firstName }} {{ user.lastName }}</option>
                   </select>
-                  <button @click="saveEditedTask(index, taskIndex)">Zapisz</button>
+                  <button @click="saveEditedTask(index, task.id)">Zapisz</button>
                   <button @click="cancelEditingTask">Anuluj</button>
                 </span>
               </li>
@@ -108,110 +105,141 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
 const newListName = ref('');
-const newTask = ref({ title: '', description: '', status: 'not-done', assignedTo: '' });
+let newTask = {
+  id: '',
+  title: '',
+  description: '',
+  status: 'not-done',
+  assignedTo: ''
+};
+
+
 const taskLists = ref([]);
 const editingTaskIndex = ref(null);
-const editedTask = ref({ title: '', description: '', status: '', assignedTo: '' });
+const editedTask = ref({ id: '', title: '', description: '', status: '', assignedTo: '' });
 const isModalVisible = ref(false);
 const isLoginVisible = ref(true);
 const loginData = ref({ email: '', password: '' });
 const registerData = ref({ firstName: '', lastName: '', email: '', password: '', nickname: '' });
 const isLoggedIn = ref(false);
 const users = ref([]);
-const userIdCounter = ref([]);
-const taskIdCounter = ref([]);
-const listIdCounter = ref([]);
+const taskIdCounter = ref(0); // Inicjalizacja licznika ID dla zadań
 
+// Metoda aktualizacji danych na serwerze
 const updateServerData = async () => {
   try {
-    await axios.post('http://localhost:3001', { taskLists: taskLists.value, users: users.value, userIdCounter: userIdCounter.value, taskIdCounter: taskIdCounter.value, listIdCounter: listIdCounter.value});
+    await axios.post('http://localhost:3001', { taskLists: taskLists.value, users: users.value });
   } catch (error) {
     console.error('Error updating data on the server', error);
   }
 };
 
+// Metoda dodawania nowej listy zadań
 const addTaskList = () => {
   if (newListName.value.trim() !== '') {
-    const idList = listIdCounter.value;
     taskLists.value.push({
-      id: idList,
       name: newListName.value,
       tasks: [],
     });
-    listIdCounter.value += 1;
     newListName.value = '';
     updateServerData();
   }
 };
 
+// Metoda usuwania listy zadań
 const removeTaskList = (index) => {
   taskLists.value.splice(index, 1);
   updateServerData();
 };
 
 const addTask = (listIndex) => {
-  if (newTask.value.title.trim() !== '') {
-    const idTask = taskIdCounter.value;
-    taskLists.value[listIndex].tasks.push({
-      id: idTask,
-      title: newTask.value.title,
-      description: newTask.value.description,
-      status: newTask.value.status,
-      assignedTo: newTask.value.assignedTo,
-    });
-    taskIdCounter.value += 1;
-    newTask.value.title = '';
-    newTask.value.description = '';
-    newTask.value.status = 'not-done';
-    newTask.value.assignedTo = '';
+  if (newTask.title.trim() !== '') {
+    const taskId = taskIdCounter.value++; // Generowanie nowego ID dla zadania
+    const task = {
+      id: taskId,
+      title: newTask.title,
+      description: newTask.description,
+      status: newTask.status,
+      assignedTo: newTask.assignedTo,
+    };
+    taskLists.value[listIndex].tasks.push(task);
+    newTask.title = ''; // Resetowanie pól nowego zadania
+    newTask.description = '';
+    newTask.status = 'not-done';
+    newTask.assignedTo = '';
     updateServerData();
   }
 };
 
-const removeTask = (listIndex, taskIndex) => {
-  taskLists.value[listIndex].tasks.splice(taskIndex, 1);
-  updateServerData();
+
+
+// Metoda usuwania zadania z listy
+const removeTask = (listIndex, taskId) => {
+  const list = taskLists.value[listIndex];
+  const taskIndex = list.tasks.findIndex(task => task.id === taskId);
+  if (taskIndex !== -1) {
+    list.tasks.splice(taskIndex, 1);
+    updateServerData();
+  }
 };
 
-const startEditingTask = (listIndex, taskIndex) => {
-  editingTaskIndex.value = taskIndex;
-  editedTask.value.title = taskLists.value[listIndex].tasks[taskIndex].title;
-  editedTask.value.description = taskLists.value[listIndex].tasks[taskIndex].description;
-  editedTask.value.status = taskLists.value[listIndex].tasks[taskIndex].status;
+// Metoda rozpoczęcia edycji zadania
+const startEditingTask = (listIndex, taskId) => {
+  const list = taskLists.value[listIndex];
+  const task = list.tasks.find(task => task.id === taskId);
+  if (task) {
+    editingTaskIndex.value = taskId;
+    editedTask.value.id = taskId;
+    editedTask.value.title = task.title;
+    editedTask.value.description = task.description;
+    editedTask.value.status = task.status;
+    editedTask.value.assignedTo = task.assignedTo;
+  }
 };
 
-const saveEditedTask = (listIndex, taskIndex) => {
-  taskLists.value[listIndex].tasks[taskIndex].title = editedTask.value.title;
-  taskLists.value[listIndex].tasks[taskIndex].description = editedTask.value.description;
-  taskLists.value[listIndex].tasks[taskIndex].status = editedTask.value.status;
-  taskLists.value[listIndex].tasks[taskIndex].assignedTo = editedTask.value.assignedTo;
-  updateServerData();
-  cancelEditingTask();
+// Metoda zapisywania edytowanego zadania
+const saveEditedTask = (listIndex, taskId) => {
+  const list = taskLists.value[listIndex];
+  const task = list.tasks.find(task => task.id === taskId);
+  if (task) {
+    task.title = editedTask.value.title;
+    task.description = editedTask.value.description;
+    task.status = editedTask.value.status;
+    task.assignedTo = editedTask.value.assignedTo;
+    updateServerData();
+    cancelEditingTask();
+  }
 };
 
+// Metoda anulowania edycji zadania
 const cancelEditingTask = () => {
   editingTaskIndex.value = null;
-  editedTask.value = { title: '', description: '', status: '', assignedTo: '' };
+  editedTask.value = { id: '', title: '', description: '', status: '', assignedTo: '' };
 };
 
+// Metoda pokazania okna logowania
 const showLoginModal = () => {
   isModalVisible.value = true;
   isLoginVisible.value = true;
 };
 
+// Metoda pokazania okna rejestracji
 const showRegisterModal = () => {
   isModalVisible.value = true;
   isLoginVisible.value = false;
 };
 
+// Metoda zamknięcia okna modalnego
 const closeModal = () => {
   isModalVisible.value = false;
 };
 
+// Metoda wylogowania użytkownika
 const logout = () => {
   isLoggedIn.value = false;
 };
 
+// Metoda logowania użytkownika
 const login = () => {
   const user = findUserByEmail(loginData.value.email);
   if (user && user.password === loginData.value.password) {
@@ -222,6 +250,7 @@ const login = () => {
   }
 };
 
+// Metoda rejestracji użytkownika
 const register = () => {
   const existingUser = findUserByEmail(registerData.value.email);
   if (!existingUser) {
@@ -241,33 +270,31 @@ const register = () => {
   }
 };
 
+// Metoda generująca kolejne unikalne ID użytkownika
 const getNextUserId = () => {
-  const id = userIdCounter.value;
-  userIdCounter.value += 1;
-  updateServerData();
+  const id = users.value.length + 1;
   return id;
 };
 
+// Metoda znajdująca użytkownika po adresie email
 const findUserByEmail = (email) => {
   return users.value.find(user => user.email === email);
 };
 
+// Metoda znajdująca użytkownika po ID
 const findUserById = (id) => {
   return users.value.find(user => user.id === id);
 };
 
+// Metoda inicjalizująca dane po załadowaniu komponentu
 onMounted(async () => {
   try {
     const response = await axios.get('http://localhost:3001');
-    userIdCounter.value = response.data.userIdCounter;
-    taskIdCounter.value = response.data.taskIdCounter;
-    listIdCounter.value = response.data.listIdCounter;
     taskLists.value = response.data.taskLists;
     users.value = response.data.users || [];
-    console.log('Users:', users.value);
+    console.log('Users:', users.value); 
   } catch (error) {
     console.error('Error fetching data', error);
   }
 });
-
 </script>
